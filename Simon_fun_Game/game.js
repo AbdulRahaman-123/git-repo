62b314a4808335d5fbbbf9b12e61d7e5187036ff
AbdulRahaman-123 <60355004+AbdulRahaman-123@users.noreply.github.com>
2025-02-22 18:46:21 -0500

    generated random colours to be selected

--- Simon_fun_Game/game.js
+++ Simon_fun_Game/game.js
@@ -1,1 +1,8 @@
+var gamePattern=[];
+buttonColours=["red","blue","green","yellow"];
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour=Math.floor(Math.random(butonColours)*4);
+    gamePattern.push(randomChosenColour);
+}
 alert("hello");
