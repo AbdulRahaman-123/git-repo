0a84214f8e5b0577042a1275a9dcfd4ab8edae47
AbdulRahaman-123 <60355004+AbdulRahaman-123@users.noreply.github.com>
2025-02-24 14:22:54 -0500

    need to fix the animation

--- Simon_fun_Game/game.js
+++ Simon_fun_Game/game.js
@@ -1,8 +1,30 @@
-var gamePattern=[];
-buttonColours=["red","blue","green","yellow"];
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColour=Math.floor(Math.random(butonColours)*4);
-    gamePattern.push(randomChosenColour);
+var userClickedPattern=[];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  console.log($("#" + randomChosenColour));
+  playSound(randomChosenColour);
 }
-alert("hello");
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+     userClickedPattern.push(userChosenColour);
+     console.log(userClickedPattern);
+       playSound(userChosenColour);
+  });
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    }, 100);
+}
